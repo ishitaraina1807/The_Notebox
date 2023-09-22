56d88e73c9f19b07963df672ff9bd32404ae668a
ishitaraina1807 <ishitaraina18@gmail.com>
2023-09-22 21:34:31 +0530

    connected to mongodb

---
+++ backend/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const mongoURI = 'mongodb://127.0.0.1:27017/notes';  
+
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB successfully');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+};
+
+module.exports = connectToMongo;
---
+++ backend/index.js
@@ -0,0 +1,3 @@
+const connectToMongo = require('./db')
+
+connectToMongo();
