9e7e0a7c663ecdcbbad6940b002a9db73399277f
ishitaraina1807 <ishitaraina18@gmail.com>
2023-09-23 14:49:45 +0530

    added components

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,13 @@
 const connectToMongo = require('./db')
+const express = require('express')
+const app = express()
+const port = 3000
 
+app.get('/', (req, res) => {
+  res.send('Hello Daddy <3')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 connectToMongo();
