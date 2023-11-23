d0a3b6f6cc1fcc86164cece60aa1025b6e466a04
ishitaraina1807 <ishitaraina18@gmail.com>
2023-11-23 16:49:03 +0530

    note items

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -4,7 +4,7 @@ const NoteItem = (props) => {
     const { note } = props;
     return (
         <div className='col-md-3'>
-            <div className="card wrap">
+            <div className="card wrap mt-4">
                 <div className="card-body">
                     <h5 className="card-title">{note.title}</h5>
                     <h6 className="card-subtitle mb-2 text-body-secondary"> {note.tag}</h6>
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -32,6 +32,24 @@ const NoteState = (props)  => {
         "tag": "Hobbies",
         "date": "2023-11-23T10:22:55.611Z",
         "__v": 0
+      },
+      {
+        "_id": "655f27cb64e4c6215b73ce17",
+        "user": "6517f15961ea9ff79e23d1db",
+        "title": "Jogging",
+        "description": "go to the park",
+        "tag": "fitness",
+        "date": "2023-11-23T10:22:03.099Z",
+        "__v": 0
+      },
+      {
+        "_id": "655f27e464e4c6215b73ce19",
+        "user": "6517f15961ea9ff79e23d1db",
+        "title": "Youtube",
+        "description": "Watch redux videos",
+        "tag": "studies",
+        "date": "2023-11-23T10:22:28.908Z",
+        "__v": 0
       }
     ]
 const [notes, setNotes] = useState(notesInitial);
