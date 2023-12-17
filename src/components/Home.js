1af0e920b3d0c521290ea5b3126b53ecf8d53b39
ishitaraina1807 <ishitaraina18@gmail.com>
2023-12-17 22:45:52 +0530

    color and home

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,14 +7,14 @@ const Home = () => {
       <h2>Add a Note</h2>
       <div className="container my-3">
       <div className="mb-3">
-        <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
-        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+        <label htmlFor="exampleFormControlInput1" className="form-label">Add Title</label>
+        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Leetcode daily quest..." />
       </div>
       <div className="mb-3">
-        <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+        <label htmlFor="exampleFormControlTextarea1" className="form-label">Add Description</label>
         <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
       </div>
-      <button type="button" className="btn btn-primary">Primary</button>
+      <button type="button" className="btn btn-dark">Add note</button>
       </div>
       <Notes/>
     </div>
